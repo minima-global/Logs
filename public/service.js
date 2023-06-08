4512526d9e6c522d338704979f815880dc1a0e73
dynamitesushi <winson.hero@gmail.com>
2023-06-08 15:25:13 +0100

    updated gitignore, create-minima-app scripts and fixed issue with service.js not replacing all "'" due to not using regexp

--- public/service.js
+++ public/service.js
@@ -4,7 +4,7 @@ MDS.init(function (msg) {
     // create schema
     MDS.sql('CREATE TABLE IF NOT EXISTS logs (id bigint auto_increment,message varchar(2048) NOT NULL,timestamp TIMESTAMP)');
   } else if (msg.event === 'MINIMALOG') {
-    var encoded = encodeURIComponent(msg.data.message).replace("'", "%27");
+    var encoded = encodeURIComponent(msg.data.message).replace(/'/g, "%27");
 
     // insert logs into the logs table
     MDS.sql(`INSERT INTO logs (message,timestamp) VALUES ('${encoded}',CURRENT_TIMESTAMP)`, function (response) {
